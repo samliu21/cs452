720cc98a7add35576267abfc40630f4122586b18
s742liu <s742liu@uwaterloo.ca>
2025-02-27 13:24:21 -0500

    stack size was 1KBgit add .!

--- include/common.h
+++ include/common.h
@@ -8,7 +8,7 @@ typedef long long int64_t;
 typedef void (*func_t)();
 
 #define NUM_TASKS 32
-#define STACK_SIZE NUM_TASKS * 1024
+#define STACK_SIZE NUM_TASKS * 1024 * 1024
 #define MAX_RAND 32767
 
 void memcpy(void* dest, const void* src, int n);
